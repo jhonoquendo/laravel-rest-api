3112b1f07960812a72356e25a02538917477a93d
jhonoquendo <32801020+jhonoquendo@users.noreply.github.com>
2021-01-24 14:14:52 -0500

    Se agrega una cuenta con saldo inicial en ruta balance

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        if($request->input('type') === 'deposit'){
+            return $this->deposit(
+                $request->input('destination'),
+                $request->input('amount')
+            );
+        }
+        //return $request->input('type');
+    }
+
+    private function deposit($destination,$amount){
+        $account = Account::firstOrCreate([
+            'id' => $destination
+        ]);
+
+        $account->balance += $amount;
+        $account->save();
+
+        return response()->json([
+            'destination' => [
+                'id' => $account->id,
+                'balance' => $account->balance
+            ]
+            ],201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\BalanceController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ResetController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/reset','ResetController@reset');
+Route::post('/reset',[ResetController::class,'reset']);
 
-Route::get('/balance','BalanceController@show');
+Route::get('/balance',[BalanceController::class,'show']);
 
-Route::get('/event','EventController@store');
+Route::post('/event',[EventController::class,'store']);
