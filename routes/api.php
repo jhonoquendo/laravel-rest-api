8b9b36563cbc728a9c50eb48bc18ebde6fabf262
jhonoquendo <32801020+jhonoquendo@users.noreply.github.com>
2021-02-07 22:28:07 -0500

    Se agrega configuracion de cors para permisos de peticiones

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\ResetController;
 use App\Http\Controllers\ListarController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/listar',[ListarController::class,'listar']);
-
 Route::post('/reset',[ResetController::class,'reset']);
-
 Route::get('/balance',[BalanceController::class,'show']);
-
 Route::post('/event',[EventController::class,'store']);
+
+
+
+
+
